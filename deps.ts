21f11f92ed0f42b95ac29a786d5d488c745f6878
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-05-29 16:32:43 -0700

    Run `deno task all`

--- deps.ts
+++ deps.ts
@@ -1,15 +1,15 @@
-export { parse } from "https://deno.land/std@0.177.1/flags/mod.ts";
+export { parse } from "https://deno.land/std@0.190.0/flags/mod.ts";
 export {
   globToRegExp,
   isAbsolute,
   isGlob,
   join,
   toFileUrl,
-} from "https://deno.land/std@0.177.1/path/mod.ts";
+} from "https://deno.land/std@0.190.0/path/mod.ts";
 export {
   getLogger,
   handlers,
   setup,
-} from "https://deno.land/std@0.177.1/log/mod.ts";
+} from "https://deno.land/std@0.190.0/log/mod.ts";
 
 export { createGraph } from "https://deno.land/x/deno_graph@0.48.1/mod.ts";
